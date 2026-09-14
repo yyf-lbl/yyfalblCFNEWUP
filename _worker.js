272812640dc9cf2489ac27b40d2b4724fb0b9f6b
yyf-lbl <yyfalbl@gmail.com>
2026-09-15 00:23:34 +0800

    Update terminal version from v3.0 to v4.0

--- _worker.js
+++ _worker.js
@@ -1016,8 +1016,8 @@ export default {
           const 语言值661 = 是否值664 ? 'fa-IR' : 'zh-CN';
           const 本地值660 = {
             zh: {
-              title: '终端 v3.0',
-              terminal: '终端 v3.0',
+              title: '终端 v4.0',
+              terminal: '终端 v4.0',
               congratulations: '恭喜你来到这',
               enterU: '请输入你U变量的值',
               enterD: '请输入你D变量的值',
